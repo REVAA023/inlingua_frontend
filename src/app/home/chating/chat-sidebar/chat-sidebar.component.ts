0bd34c5f41b8d94fe18537332f36379cbf2c0b90
Vignesh <157564464+REVAA023@users.noreply.github.com>
2025-04-12 13:30:37 +0530

    chat

--- src/app/home/chating/chat-sidebar/chat-sidebar.component.ts
+++ src/app/home/chating/chat-sidebar/chat-sidebar.component.ts
@@ -1,14 +1,192 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-chat-sidebar',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './chat-sidebar.component.html',
-  styleUrl: './chat-sidebar.component.scss'
+  styleUrl: './chat-sidebar.component.scss',
 })
-export class ChatSidebarComponent {
-items: any = [
- 1, 2, 3, 4, 5, 6, 7, 8
-];
+export class ChatSidebarComponent implements OnInit {
 
+  chatConfig = "";
+
+  items: any = [
+    {
+      id: 1,
+      name: 'Sam Kumar',
+      email: 'samkumar@gmail.com',
+      role: 'Trainer',
+      image: 'assets/images/user1.png',
+      lastMessage:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero deserunt ipsa placeat fugiat. Veritatis, fugiat distinctio! Temporibus cum, debitis suscipit, beatae laudantium expedita officiis voluptate recusandae placeat provident animi!',
+      lastMessageTime: '10:30 AM',
+      unreadMessages: 0,
+      messages: [
+        {
+          id: 1,
+          auther: 'Sam Kumar',
+          message: 'Hello',
+          time: '10:30 AM',
+          replayId: 0,
+        },
+        { id: 2, auther: 'You', message: 'Hi', time: '10:30 AM', replayId: 1 },
+        {
+          id: 3,
+          auther: 'You',
+          message: 'Sorry',
+          time: '10:30 AM',
+          replayId: 0,
+        },
+        {
+          id: 4,
+          auther: 'Sam Kumar',
+          message: 'No Problem',
+          time: '10:30 AM',
+          replayId: 3,
+        },
+        {
+          id: 5,
+          auther: 'Sam Kumar',
+          message: 'How are you?',
+          time: '10:30 AM',
+          replayId: 0,
+        },
+        {
+          id: 6,
+          auther: 'You',
+          message: 'I am fine',
+          time: '10:30 AM',
+          replayId: 5,
+        },
+        {
+          id: 7,
+          auther: 'You',
+          message: 'How are you?',
+          time: '10:30 AM',
+          replayId: 0,
+        },
+        {
+          id: 8,
+          auther: 'Sam Kumar',
+          message: 'I am fine',
+          time: '10:30 AM',
+          replayId: 7,
+        },
+        {
+          id: 9,
+          auther: 'Sam Kumar',
+          message: 'How is your family?',
+          time: '10:30 AM',
+          replayId: 0,
+        },
+        {
+          id: 10,
+          auther: 'You',
+          message: 'They are fine',
+          time: '10:30 AM',
+          replayId: 9,
+        },
+        {
+          id: 11,
+          auther: 'You',
+          message: 'How is your family?',
+          time: '10:30 AM',
+          replayId: 0,
+        },
+        {
+          id: 12,
+          auther: 'Sam Kumar',
+          message: 'They are fine',
+          time: '10:30 AM',
+          replayId: 11,
+        },
+        {
+          id: 13,
+          auther: 'Sam Kumar',
+          message: 'Ok Bye',
+          time: '10:30 AM',
+          replayId: 0,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Ravi Kumar',
+      email: 'ravi@gmail.com',
+      role: 'Student',
+      image: 'assets/images/user1.png',
+      lastMessage:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero deserunt ipsa placeat fugiat. Veritatis, fugiat distinctio! Temporibus cum, debitis suscipit, beatae laudantium expedita officiis voluptate recusandae placeat provident animi!',
+      lastMessageTime: '10:35 AM',
+      unreadMessages: 2,
+      messages: [],
+    },
+    {
+      id: 3,
+      name: 'Allwin',
+      email: 'allwin@gmail.com',
+      role: 'Student',
+      image: 'assets/images/user1.png',
+      lastMessage:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero deserunt ipsa placeat fugiat. Veritatis, fugiat distinctio! Temporibus cum, debitis suscipit, beatae laudantium expedita officiis voluptate recusandae placeat provident animi!',
+      lastMessageTime: '10:40 AM',
+      unreadMessages: 0,
+      messages: [],
+    },
+    {
+      id: 4,
+      name: 'Raabin',
+      email: 'raabin@gmail.com',
+      role: 'Student',
+      image: 'assets/images/user1.png',
+      lastMessage:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero deserunt ipsa placeat fugiat. Veritatis, fugiat distinctio! Temporibus cum, debitis suscipit, beatae laudantium expedita officiis voluptate recusandae placeat provident animi!',
+      lastMessageTime: '10:45 AM',
+      unreadMessages: 3,
+      messages: [],
+    },
+    {
+      id: 5,
+      name: 'Sathish',
+      email: 'sathish@gmail.com',
+      role: 'Student',
+      image: 'assets/images/user1.png',
+      lastMessage:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero deserunt ipsa placeat fugiat. Veritatis, fugiat distinctio! Temporibus cum, debitis suscipit, beatae laudantium expedita officiis voluptate recusandae placeat provident animi!',
+      lastMessageTime: '10:50 AM',
+      unreadMessages: 8,
+      messages: [],
+    },
+    {
+      id: 6,
+      name: 'Annatha',
+      email: 'annatha@gmail.com',
+      role: 'Student',
+      image: 'assets/images/user1.png',
+      lastMessage:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero deserunt ipsa placeat fugiat. Veritatis, fugiat distinctio! Temporibus cum, debitis suscipit, beatae laudantium expedita officiis voluptate recusandae placeat provident animi!',
+      lastMessageTime: '10:29 AM',
+      unreadMessages: 100,
+      messages: [],
+    },
+  ];
+
+  @Output() onClick = new EventEmitter<any>();
+
+  constructor() {}
+  onChatClick(user: any) {
+    console.log(user);
+    this.chatConfig = user.email;
+    this.onClick.emit(user);
+    this.items.forEach((item: any) => {
+      if (item.id == user.id) {
+        item.unreadMessages = 0;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    console.log(this.items);
+  }
 }
--- src/app/home/chating/chating.component.ts
+++ src/app/home/chating/chating.component.ts
@@ -3,12 +3,41 @@ import { ChatSidebarComponent } from './chat-sidebar/chat-sidebar.component';
 
 @Component({
   selector: 'app-chating',
-  imports: [
-    ChatSidebarComponent
-  ],
+  imports: [ChatSidebarComponent],
   templateUrl: './chating.component.html',
-  styleUrl: './chating.component.scss'
+  styleUrl: './chating.component.scss',
 })
 export class ChatingComponent {
+  isChatEnabled: boolean = false;
+  isChatSidebarEnabled: boolean = false;
 
+  selectedChat: any = {
+    id: 0,
+    name: '',
+    role: '',
+    image: '',
+    lastMessage: '',
+    lastMessageTime: '',
+    unreadMessages: 0,
+    messages: [],
+  };
+
+  getChatingDetail(event: any) {
+    console.log(event);
+    this.selectedChat = event;
+    this.isChatEnabled = true;
+
+    setTimeout(() => {
+      this.addMessage()
+    }, 10);
+  }
+
+  addMessage() {
+    const chatBar = document.querySelector('.chat-messages') as HTMLElement;
+
+    // Scroll to bottom
+    chatBar.scrollTop = chatBar.scrollHeight;
+
+    chatBar.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }
 }
